8b490bbfd379ca275a1a0363f89a0aa23e441e7a
Kaela Schaefer <kaelaxxanne@gmail.com>
2017-07-14 18:29:03 -0700

    Updating cron job

--- manifests/site.pp
+++ manifests/site.pp
@@ -7,7 +7,7 @@ node ip-10-0-7-136 {
 node ip-10-0-7-222 {
 	cron { "puppet update":
 		command => "cd /etc/puppet && git pull -q origin master",
-		user => 'root',
+		user => "root",
 		minute => "*/5",
 	}	
 }
