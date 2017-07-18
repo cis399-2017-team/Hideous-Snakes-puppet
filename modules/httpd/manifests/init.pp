e9c183888f6c6fb193932693c680cf207f2cddb7
Kaela Schaefer <kaelaxxanne@gmail.com>
2017-07-17 21:45:59 -0700

    Further change init.pp to include correct package name

--- modules/httpd/manifests/init.pp
+++ modules/httpd/manifests/init.pp
@@ -6,7 +6,7 @@ class { "httpd":
 	service { "httpd":
 		ensure => running,
 		enable => true,
-		require => [ Package['apache'], File["/etc/apache2/httpd.conf"] ],
+		require => [ Package['apache2'], File["/etc/apache2/httpd.conf"] ],
 		subscribe => File["/etc/apache2/httpd.conf"],
 		hasstatus => true,
 		hasrestart => true,
@@ -19,6 +19,6 @@ class { "httpd":
 		mode => 0640,
 		owner => root,
 		group => root,
-		require => Package['apache'],
+		require => Package['apache2'],
 	}	
 }
