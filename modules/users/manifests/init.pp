47a1368ad9508677265ad6ca60d2b56e42f79a0b
Nate Boyd <nboyd@uoregon.edu>
2017-07-25 15:16:11 -0700

    added ssh key stuff

--- modules/users/manifests/init.pp
+++ modules/users/manifests/init.pp
@@ -1,12 +1,20 @@
 ## Template for later use!!!##
 class users {
-   user { 'fake':
-      ensure  => 'present',
-      comment => 'Fake Fakenstine,,,',
-      gid     => '1001',
-      groups  => ['sudo', 'video'],
-      home    => '/home/fake',
-      shell   => '/bin/bash',
-      uid     => '1001',
-    }
+	user { 'fakef':
+		ensure  => 'present',
+		comment => 'Fake Fakenstine,,,',
+		gid     => '1001',
+		groups  => ['sudo', 'video'],
+		home    => '/home/fake',
+		shell   => '/bin/bash',
+		uid     => '1001',
+		purge_ssh_keys => true,
+	}
+	ssh_authorized_key {
+		ensure  => 'present',
+		type    => 'ssh-rsa',
+		user    => 'fake',
+		key     => 'insertRealKeyHere',
+		
+	}
 }
