c69822b6721c6c2184ec86b000f0970e4fa351eb
Kaela Schaefer <kaelaxxanne@gmail.com>
2017-08-12 13:10:59 -0700

    Add resources to init.pp

--- modules/fakeFreeze/manifests/init.pp
+++ modules/fakeFreeze/manifests/init.pp
@@ -3,4 +3,16 @@ class fakeFreeze {
 		ensure => latest,
 	}
 	
+	file { "helloWorld.c":
+		path => INSERT SOMETHING HERE,
+		source => "puppet:///modules/fakeFreeze/helloWorld.c",
+		mode => 0777,
+		owner => root,
+		group => root,
+		require => Package['build-essentials'],
+	}	
+	
+	package { "build-essentials":
+		ensure => installed,
+	}
 }
