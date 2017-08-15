4b7cd136c4503f02f5003971ce10cd39cf321b13
Kaela Schaefer <kaelaxxanne@gmail.com>
2017-08-15 15:16:49 -0700

    Fixing errors with fakeFreeze

--- modules/fakeFreeze/manifests/init.pp
+++ modules/fakeFreeze/manifests/init.pp
@@ -1,17 +1,17 @@
 class fakeFreeze {
-	cron {"clean fake1":
-                command => "find /tmp -user fake1 -delete && find /var/tmp -user fake1 -delete",
-                user => "root",
-                hour => "23",
+  cron { 'clean fake1':
+                command => 'find /tmp -user fake1 -delete && find /var/tmp -user fake1 -delete',
+                user => 'root',
+                hour => '23',
         }
-	cron {"clean fake2":
-                command => "find /tmp -user fake2 -delete && find /var/tmp -user fake2 -delete",
-                user => "root",
-                hour => "23",
+  cron { 'clean fake2':
+                command => 'find /tmp -user fake2 -delete && find /var/tmp -user fake2 -delete',
+                user => 'root',
+                hour => '23',
         }
-	cron {"clean fake3":
-                command => "find /tmp -user fake3 -delete && find /var/tmp -user fake3 -delete",
-                user => "root",
-                hour => "23",
+  cron { 'clean fake3':
+                command => 'find /tmp -user fake3 -delete && find /var/tmp -user fake3 -delete',
+                user => 'root',
+                hour => '23',
         }
 }
