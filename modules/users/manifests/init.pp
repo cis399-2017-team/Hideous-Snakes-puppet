6a75350ca5b6141a3284593986e49ae7239c31d3
Nate Boyd <nboyd@uoregon.edu>
2017-08-14 17:35:49 -0700

    messing with user rights

--- modules/users/manifests/init.pp
+++ modules/users/manifests/init.pp
@@ -5,20 +5,50 @@ class users {
 		 ensure => 'present',
   	}
 
-	user { 'fakef':
+	user { 'fake1':
 		ensure  => 'present',
-		comment => 'Fake Fakenstine',
 		groups  => ['classmate'],
 		shell   => '/bin/bash',
         	purge_ssh_keys => true,
 	    	managehome => true,
-		home   => '/home/fakef',
+		home   => '/home/fake1',
+		uid    => '1111',
 	}
 	ssh_authorized_key {'Fake Fakenstine':
 		type    => 'ssh-rsa',
-		user    => 'fakef',
+		user    => 'fake1',
 		key     => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDTXfzIWZlGglEQu9vr3gaMYg4We34PHk2S1oejSEEiRvlVALdDXRuHp0s3mqAPgL9ClQhvefntWgwwNQ4jCyfvYxZfw8+gHvS8HjyszbwCNnVfk3y1k4fUyo+HrRmX9jow1Qkf0Ut/YFsx9RT0dOr+bQ5w/2YOjLwb78OyGkh48Sb4ucPhZhMmmr7KlD6wC66XNWG9WvozHwsHXcR8ZLQjEAfb5LQ3Tdt2Gf7ykMBHapF1nyk7yoPJvMuyhgJV3uRSsmxYrwOJyQOhzY8PHl3AplY6DqcvnXbvsxe0uJxHaVBHWVCyk7AIcGHs8TfRTtezP8umajrB2ZAC9SKwQGL/',
 	}
+
+	user { 'fake2':
+	        ensure  => 'present',
+                groups  => ['classmate'],
+                shell   => '/bin/bash',
+                purge_ssh_keys => true,
+                managehome => true,
+                home   => '/home/fake2',
+                uid    => '1111',
+        }
+        ssh_authorized_key {'Fake Fakenstine':
+                type    => 'ssh-rsa',
+                user    => 'fake2',
+                key     => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDTXfzIWZlGglEQu9vr3gaMYg4We34PHk2S1oejSEEiRvlVALdDXRuHp0s3mqAPgL9ClQhvefntWgwwNQ4jCyfvYxZfw8+gHvS8HjyszbwCNnVfk3y1k4fUyo+HrRmX9jow1Qkf0Ut/YFsx9RT0dOr+bQ5w/2YOjLwb78OyGkh48Sb4ucPhZhMmmr7KlD6wC66XNWG9WvozHwsHXcR8ZLQjEAfb5LQ3Tdt2Gf7ykMBHapF1nyk7yoPJvMuyhgJV3uRSsmxYrwOJyQOhzY8PHl3AplY6DqcvnXbvsxe0uJxHaVBHWVCyk7AIcGHs8TfRTtezP8umajrB2ZAC9SKwQGL/',
+        }
+
+        user { 'fake3':
+                ensure  => 'present',
+                groups  => ['classmate'],
+                shell   => '/bin/bash',
+                purge_ssh_keys => true,
+                managehome => true,
+                home   => '/home/fake3',
+                uid    => '1111',
+        }
+        ssh_authorized_key {'Fake Fakenstine':
+                type    => 'ssh-rsa',
+                user    => 'fake3',
+                key     => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDTXfzIWZlGglEQu9vr3gaMYg4We34PHk2S1oejSEEiRvlVALdDXRuHp0s3mqAPgL9ClQhvefntWgwwNQ4jCyfvYxZfw8+gHvS8HjyszbwCNnVfk3y1k4fUyo+HrRmX9jow1Qkf0Ut/YFsx9RT0dOr+bQ5w/2YOjLwb78OyGkh48Sb4ucPhZhMmmr7KlD6wC66XNWG9WvozHwsHXcR8ZLQjEAfb5LQ3Tdt2Gf7ykMBHapF1nyk7yoPJvMuyhgJV3uRSsmxYrwOJyQOhzY8PHl3AplY6DqcvnXbvsxe0uJxHaVBHWVCyk7AIcGHs8TfRTtezP8umajrB2ZAC9SKwQGL/',
+        }
 	
 	user { 'stevev':
 		ensure  => 'present',
