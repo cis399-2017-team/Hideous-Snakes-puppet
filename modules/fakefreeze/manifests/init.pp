40f7d8121ad394166bc3d13ea50c2ba686285e82
Nate Boyd <nboyd@uoregon.edu>
2017-08-17 19:21:53 -0700

    testing stuff

--- modules/fakefreeze/manifests/init.pp
+++ modules/fakefreeze/manifests/init.pp
@@ -4,7 +4,8 @@ class fakefreeze {
   cron { 'clean fake1':
     command => 'find /tmp -user fake1 -delete && find /var/tmp -user fake1 -delete',
     user => 'root',
-    hour => '23',
+    ### hour => '23',
+    minute => '*/3',
   }
   cron { 'clean fake2':
     command => 'find /tmp -user fake2 -delete && find /var/tmp -user fake2 -delete',
